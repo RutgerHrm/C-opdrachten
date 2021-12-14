a6b5a1bf5fea7bac9c13a0639399d46682f5829a
Rutger <78957676+Stinkboer@users.noreply.github.com>
2021-12-14 20:45:53 +0100

    Update opdracht3c.c

--- opdracht3c.c
+++ opdracht3c.c
@@ -4,41 +4,43 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct{
+    char naam[20];
+    int instroomjaar;
+} opleiding;
 
-/* In de main worden antwoorden afgelezen met een for loop */
+/* Geneste structure */
+typedef struct structmain{
+    char naam[20]; /* Naam mag dus max 20 karakters lang zijn */
+    int leeftijd;
+    opleiding opleiding;
+} structmain;
+
+/* In main antwoorden aflezen dmv for loop */
 int main()
 {
     int i;
-    struct main{
-    char naam[10];
-    int leeftijd;
-      
-    struct opleiding{
-    char naam[10];
-    int instroomjaar;
-}   
-    opleiding;
-};
-    struct main x[4];
-    for (i = 0; i < 4; i++)
+    struct structmain questions[4];
+    for (i = 0; i < 1; i++)
     {
-        printf("Hoe heet je?   ");
-        scanf("%s", x[i].naam);
+        printf("Hoe heet u?   ");
+        scanf("%s", &questions[i].naam); /* %s = string */
         printf("Welke opleiding ben je aan het doen?   ");
-        scanf("%s", x[i].opleiding);
+        scanf("%s", &questions[i].opleiding.naam);
         printf("Hoe oud ben je?   ");
-        scanf("%d", &x[i].leeftijd);
+        scanf("%d", &questions[i].leeftijd); /* %d = int/digit */
         printf("Welk jaar ben jij ingestroomd?   ");
-        scanf("%d", &x[i].opleiding.instroomjaar);
+        scanf("%d", &questions[i].opleiding.instroomjaar);
         printf("\n");
     }
-    
-    printf("Dit zijn uw antwoorden:\n");
-    for (i = 0; i < 4; i++)
-    {
-        printf("%f\n%d\n%s\n%f\n\n", 
 
-        x[i].naam, x[i].opleiding, x[i].leeftijd, x[i].opleiding.instroomjaar);
+    /* Antwoorden printen dmv for loop */
+    for (i = 0; i < 1; i++)
+    {
+        printf("Uw naam - %s   \n", questions[i].naam);
+        printf("Uw opleiding - %s   \n", questions[i].opleiding.naam);
+        printf("Uw leeftijd - %d   \n", questions[i].leeftijd);
+        printf("Uw opleidingsinstroomjaar - %d   \n", questions[i].opleiding.instroomjaar);
     }
     return(0);
 }
