101a0c988a35392cbe941a3ee799d45ee797ee8a
Rutger <78957676+Stinkboer@users.noreply.github.com>
2021-12-14 21:03:06 +0100

    Update opdracht4b.c

--- opdracht4b.c
+++ opdracht4b.c
@@ -1,1 +1,5 @@
 
+/*
+ * 4b is mij niet meer gelukt. Plagiaat doe ik niet dus ik houd het hier voorlopig leeg, ik heb liever dat de opdracht met antwoord duidelijk wordt
+ * uitgelegd in de les zodat ik het snap.
+ */
